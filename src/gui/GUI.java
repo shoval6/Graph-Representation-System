b5182f39595829239f3eb78ad587aef75edb909f
shoval6 <shovalhm@gmail.com>
2020-02-25 09:47:35 +0200

    add initGUI function

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -13,8 +13,19 @@ import javax.swing.KeyStroke;
 public class GUI extends JFrame implements ActionListener {
 
 	public GUI() {
+		initGUI();
+		initMenu();
 		
 	}
+	
+	private void initGUI() {
+		setTitle("Graph-Represenation");
+		setSize(1200, 600);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+		setLocationRelativeTo(null);
+	}
+	
 	
 	public static void main(String[] args) {
 		GUI gui = new GUI();
