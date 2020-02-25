4e4edd7e39a96bb01f8a148befaadb8a7af53300
shoval6 <shovalhm@gmail.com>
2020-02-25 09:43:55 +0200

    create GUI class

---
+++ src/gui/GUI.java
@@ -0,0 +1,23 @@
+package gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
+
+public class GUI extends JFrame implements ActionListener {
+
+	
+	public static void main(String[] args) {
+		GUI gui = new GUI();
+		
+	}
+	
+}
+
+
