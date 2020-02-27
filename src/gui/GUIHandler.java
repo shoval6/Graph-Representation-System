05917fa1311a0373ab6a16dc981ebf077ee57069
shoval6 <shovalhm@gmail.com>
2020-02-27 18:18:02 +0200

    add addEdge() function

--- src/gui/GUIHandler.java
+++ src/gui/GUIHandler.java
@@ -1,26 +1,32 @@
 package gui;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.Iterator;
+
 import javax.swing.JOptionPane;
 
 import algorithms.Graph_Algo;
 import algorithms.graph_algorithms;
 import dataStructure.DGraph;
 import dataStructure.Node;
+import dataStructure.node_data;
 import utils.Point3D;
 
 public class GUIHandler {
 
 	public static GUIHandler guiHandler = new GUIHandler();
-	private graph_algorithms graph;
-	private DGraph g;
+	private graph_algorithms graphAlgo;
+	private DGraph graph;
 	final int X_COORD = 60;
-	final int Y_COORD = 700;
+	final int Y_COORD = 720;
 	
 	public GUIHandler() {
-		g = new DGraph();
-		graph = new Graph_Algo();
-		graph.init(g);
+		graph = new DGraph();
+		graphAlgo = new Graph_Algo();
+		graphAlgo.init(graph);
 	}
+	
 	
 	public void addNode() {
 		String xCoord = JOptionPane.showInputDialog("Enter x coordinate between 1-65");
@@ -28,18 +34,26 @@ public class GUIHandler {
 		Point3D point = new Point3D(Double.parseDouble(xCoord),Double.parseDouble(yCoord));
 		Node node = new Node(point);
 		calcNodePosition(node);
+		graph.addNode(node);
 		
 	}
 	
+	public void addEdge() {
+		String nodeSrc = JOptionPane.showInputDialog("Enter node source number");
+		String nodeDest = JOptionPane.showInputDialog("Enter node destination number");
+		String weight = JOptionPane.showInputDialog("Enter weight");
+		graph.connect(Integer.parseInt(nodeSrc), Integer.parseInt(nodeDest), Integer.parseInt(weight));
+	}
+	
 	public void calcNodePosition(Node node) {
-		double x = X_COORD + (node.getLocation().ix()*20);
-		double y = Y_COORD + (node.getLocation().iy()*20);
+		double x = X_COORD + (node.getLocation().ix()*20)-5;
+		double y = Y_COORD - (node.getLocation().iy()*20)+5;
 		Point3D point = new Point3D(x,y);
 		node.setLocation(point);
 	}
 	
 	public int getMC() {
-		return this.g.getMC();
+		return this.graph.getMC();
 	}
 	
 }
